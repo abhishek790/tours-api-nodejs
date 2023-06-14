3cb1b093801218e162f9f79c3e039c9c49bdd113
Abhishek Mishra <mishraavi790@gmail.com>
2023-06-14 22:54:29 +0545

    responding to url parameter

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const express = require('express');
 
 const app = express();
 
-// we use app.use to use middleware
-// this express.josn() is a middleware, middleware is just a function that can modify the incoming request data. It's called middleware because it stands between in the middle of request and response. so it's just a steps request goes through while being processed
-// and the steps the request goes through in this example is simply that the data form the body is added to the request object
-app.use(express.json());
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
@@ -22,12 +17,39 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// what we want to implement in this lecture is a way of getting only one tour
+// define a route that can accept a variable
+// :id => we created variable id
+// we can also define multiple variable
+//:y?=> this makes that parameter optional (meaning if we didnot specify in the 3rd parameter, it will still work)
+app.get('/api/v1/tours/:id', (req, res) => {
+  // these variable in the url are called parameters
+  // param is where the all the parameters,all the variables that we define in the url are stored
+  console.log(req.params);
+  const id = Number(req.params.id);
+  // what find will do is , it will only create an array where below comparison is true
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length)
+  if (!tour) {
+    // return exit the function immediately
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body);
-
   const newId = tours[tours.length - 1].id + 1;
-
-  // allows us to create a new object by merging two existing objects
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
   fs.writeFile(
@@ -48,3 +70,7 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}... `);
 });
+
+// easy way to define parameters and right in the url and then how to read these parameters and how to respond to that
+
+// what we want to implement in this lecture is a way of getting only one tour
